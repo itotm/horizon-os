00d6175a69756865452bc776d7d554923a8ae0fa
Fabrizio Ferrari <fabrizio.ferrari@e.email>
2025-11-10 18:10:16 +0100

    Based on Kinoite

--- Containerfile
+++ Containerfile
@@ -3,7 +3,7 @@ FROM scratch AS ctx
 COPY build_files /
 
 # Base Image
-FROM ghcr.io/ublue-os/kinoite-main:latest
+FROM quay.io/fedora/fedora-kinoite:latest
 
 ### [IM]MUTABLE /opt
 ## Some bootable images, like Fedora, have /opt symlinked to /var/opt, in order to
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,35 +2,77 @@
 
 set -ouex pipefail
 
-SERVER_PACKAGES=(
+INSTALL_PACKAGES=(
+    cascadia-fonts-all
     mc
+    htop
+    fastfetch
+    hwinfo
+    kate
     hunspell-it
-    qemu
+    qemu-kvm
+    cifs-utils
     cockpit
     cockpit-machines
     cockpit-podman
-    fastfetch
+    distrobox
     syncthing
     tailscale
-    gimp
-    libreoffice
     thunderbird
     qt6-qdbusviewer
 )
+dnf5 install -y "${INSTALL_PACKAGES[@]}"
 
-dnf5 install -y "${SERVER_PACKAGES[@]}"
+REMOVE_PACKAGES=(
+    plasma-discover-rpm-ostree
+)
+dnf5 remove -y "${REMOVE_PACKAGES[@]}"
 
+# RPM Fusion
+dnf5 install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 install rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
 
-dnf5 -y copr enable lizardbyte/stable
-#dnf5 -y install Sunshine
-dnf5 search Sunshine
-dnf5 -y copr disable lizardbyte/stable
+dnf5 install intel-media-driver
+dnf5 install libdvdcss
 
+dnf5 remove \
+    fdk-aac-free \
+    libavcodec-free \
+    libavdevice-free \
+    libavfilter-free \
+    libavformat-free \
+    libavutil-free \
+    libpostproc-free \
+    libswresample-free \
+    libswscale-free \
+    ffmpeg-free
+dnf5 install ffmpeg
+
+dnf5 install \
+    gstreamer1-plugin-libav \
+    gstreamer1-plugins-bad-free-extras \
+    gstreamer1-plugins-bad-freeworld \
+    gstreamer1-plugins-ugly \
+    gstreamer1-vaapi
+
+# Sunshine
+dnf5 -y copr enable lizardbyte/beta
+dnf5 -y install Sunshine
+dnf5 -y copr disable lizardbyte/beta
 
 systemctl enable podman.socket
 systemctl enable cockpit.socket
 
+systemctl enable sshd
+systemctl enable tailscaled
 
+# Bash configuration
+cat > /etc/inputrc <<EOF
+set completion-ignore-case On
+EOF
+
+# Info
 BUILD_DATE=$(date +'%Y%m%d')
 cat > /etc/xdg/kcm-about-distrorc <<EOF
 [General]
@@ -38,3 +80,5 @@ Variant=HorizonOS ${BUILD_DATE}
 Website=https://github.com/itotm/horizon-os
 END
 EOF
+
+dnf5 clean all
