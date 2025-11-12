87f35e90bfdfe280787fa8ae1230caf8c35805e1
itotm <1804363+itotm@users.noreply.github.com>
2025-11-12 23:12:22 +0100

    tentativo

--- build_scripts/wine.sh
+++ build_scripts/wine.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -ouex pipefail
 
-# wine needs /opt writable
-rm /opt && mkdir /opt
-
 dnf5 -y config-manager addrepo --from-repofile=https://dl.winehq.org/wine-builds/fedora/43/winehq.repo
 dnf5 -y install winehq-staging
