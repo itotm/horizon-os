cb97a32f5de6119fc0a7667ddbcb0be461ebbce2
Fabrizio Ferrari <fabrizio.ferrari@e.email>
2025-11-13 22:57:03 +0100

    version+build

--- build_scripts/end.sh
+++ build_scripts/end.sh
@@ -5,7 +5,7 @@ sed -i 's/^enabled=.*/enabled=0/' /etc/yum.repos.d/*.repo
 
 BUILD_DATE=$(date +'%Y%m%d')
 BUILD_NUMBER=${BUILD_NUMBER:-1}
-VERSION="${BUILD_DATE}.${BUILD_NUMBER}"
+VERSION="v${BUILD_DATE}.${BUILD_NUMBER}"
 
 cat > /etc/xdg/kcm-about-distrorc <<EOF
 [General]
