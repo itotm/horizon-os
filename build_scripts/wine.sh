4dc77c686535c95dca49cc687ee4d7360fa906a4
itotm <1804363+itotm@users.noreply.github.com>
2025-11-13 12:54:07 +0100

    winetricks

--- build_scripts/wine.sh
+++ build_scripts/wine.sh
@@ -5,5 +5,5 @@ dnf5 -y config-manager addrepo --from-repofile=https://dl.winehq.org/wine-builds
 dnf5 -y install winehq-staging
 
 wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
-chmod +x winetricks
-mv winetricks /usr/local/bin/winetricks
+chmod +x ./winetricks
+mv ./winetricks /usr/local/bin/winetricks
