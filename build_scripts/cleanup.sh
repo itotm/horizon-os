56b405a74c4ecc2600f68301c41082a732e18b2c
Fabrizio Ferrari <fabrizio.ferrari@e.email>
2025-11-13 16:45:25 +0100

    correction

--- build_scripts/cleanup.sh
+++ build_scripts/cleanup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 set -ouex pipefail
 
+# Version info
+BUILD_DATE=$(date +'%Y%m%d')
+BUILD_NUMBER=${BUILD_NUMBER:-1}
+VERSION="${BUILD_DATE}.${BUILD_NUMBER}"
+
+cat > /etc/xdg/kcm-about-distrorc <<EOF
+[General]
+Variant=HorizonOS ${VERSION}
+Website=https://github.com/itotm/horizon-os
+EOF
+
+# Save version info for reference
+echo "${VERSION}" > /etc/horizon-version
+
 dnf5 -y clean all
 sed -i 's/^enabled=.*/enabled=0/' /etc/yum.repos.d/*.repo
