b98786d81abdc7a5734bb9ede60852c668b89ce6
Fabrizio Ferrari <fabrizio.ferrari@e.email>
2025-11-13 12:34:46 +0100

    winetricks

--- build_scripts/wine.sh
+++ build_scripts/wine.sh
@@ -2,4 +2,8 @@
 set -ouex pipefail
 
 dnf5 -y config-manager addrepo --from-repofile=https://dl.winehq.org/wine-builds/fedora/43/winehq.repo
-dnf5 -y install winehq-staging winetricks
+dnf5 -y install winehq-staging
+
+wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+chmod +x winetricks
+mv winetricks /usr/local/bin/winetricks
