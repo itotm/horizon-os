57a895db34adf2cf173deacbecbdec537c4f1a56
Fabrizio Ferrari <fabrizio.ferrari@e.email>
2025-11-13 12:20:51 +0100

    wine

--- build_scripts/wine.sh
+++ build_scripts/wine.sh
@@ -3,4 +3,3 @@ set -ouex pipefail
 
 dnf5 -y config-manager addrepo --from-repofile=https://dl.winehq.org/wine-builds/fedora/43/winehq.repo
 dnf5 -y install winehq-staging
-dnf5 -y config-manager setopt winehq.enabled=1
