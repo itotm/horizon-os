3e672834da2d6adb53167fd33dd0cf00bbf35882
itotm <1804363+itotm@users.noreply.github.com>
2025-11-13 22:44:37 +0100

    correction

--- build_scripts/start.sh
+++ build_scripts/start.sh
@@ -12,5 +12,5 @@ EOF
 ./ctx/download_github.sh https://github.com/itotm/plasma-colors/tree/main/color-schemes /usr/share/color-schemes
 ./ctx/download_github.sh https://github.com/itotm/plasma-colors/tree/main/ClearSimple /usr/share/plasma/desktoptheme/ClearSimple
 ./ctx/download_github.sh https://github.com/itotm/plasma-colors/tree/main/colored-plasma-logo /usr/share/plasma/look-and-feel/colored-plasma-logo
-./ctx/download_github.sh https://github.com/itotm/plymouth-themes/tree/main/fedora-logo /usr/share/plymouth/themes/fedora-logo
+./ctx/download_github.sh https://github.com/itotm/plymouth-themes/tree/main/fedora-logo/fedora-logo /usr/share/plymouth/themes/fedora-logo
 
