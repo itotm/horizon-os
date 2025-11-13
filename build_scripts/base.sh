7ded91ad7e06f4760313c435ffdd0082feef6f35
Fabrizio Ferrari <fabrizio.ferrari@e.email>
2025-11-13 16:32:32 +0100

    varie

--- build_scripts/base.sh
+++ build_scripts/base.sh
@@ -21,3 +21,10 @@ EOF
 
 # Save version info for reference
 echo "${VERSION}" > /etc/horizon-version
+
+# Colors, themes
+./download_github.sh https://github.com/itotm/plasma-colors/tree/main/color-schemes /usr/share/color-schemes
+./download_github.sh https://github.com/itotm/plasma-colors/tree/main/ClearSimple /usr/share/plasma/desktoptheme/ClearSimple
+./download_github.sh https://github.com/itotm/plasma-colors/tree/main/colored-plasma-logo /usr/share/plasma/look-and-feel/colored-plasma-logo
+./download_github.sh https://github.com/itotm/plymouth-themes/tree/main/fedora-logo /usr/share/plymouth/themes/fedora-logo
+
