c35adabd93255360b21050a5d6f12a66b7c819f9
itotm <1804363+itotm@users.noreply.github.com>
2025-11-14 08:20:50 +0100

    Add Visual Studio Code installation option to build workflow

---
+++ build_scripts/vscode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ouex pipefail
+
+# Import Microsoft GPG key and add repository
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+# Update package cache and install VS Code
+dnf5 check-update
+dnf5 -y install code
+
+./ctx/cleanup.sh
