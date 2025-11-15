ca1bf741390cb453232d4ebf11fb918821203f4f
itotm <1804363+itotm@users.noreply.github.com>
2025-11-15 06:46:30 +0100

    Update dotnet and vscode installation scripts: streamline package installation and remove unnecessary repository import

--- build_scripts/devtools/dotnet.sh
+++ build_scripts/devtools/dotnet.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -ouex pipefail
 
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[packages-microsoft-com-prod]\nname=Microsoft .NET Core SDK\nbaseurl=https://packages.microsoft.com/yumrepos/packages-microsoft-com-prod\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/dotnet-sdk.repo > /dev/null
-
-dnf5 check-update
-dnf5 -y install dotnet-sdk-8.0
+DOTNET_PACKAGES=(
+    glibc
+    libgcc
+    ca-certificates
+    openssl-libs
+    libstdc++
+    libicu
+    tzdata
+    krb5-libs
+    zlib
+    dotnet-sdk-10.0
+)
+dnf5 -y install "${DOTNET_PACKAGES[@]}"
 
 ./ctx/cleanup.sh
--- build_scripts/devtools/vscode.sh
+++ build_scripts/devtools/vscode.sh
@@ -2,7 +2,7 @@
 set -ouex pipefail
 
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 dnf5 check-update
 dnf5 -y install code
