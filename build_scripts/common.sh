a3d961cdc8e61230046a744a1c528a108a4c3f83
itotm <1804363+itotm@users.noreply.github.com>
2025-11-25 06:53:55 +0100

    Refactor build scripts: update image version handling and suppress output for download commands

--- build_scripts/common.sh
+++ build_scripts/common.sh
@@ -6,15 +6,15 @@ if [ "${DISABLE_REPOS:-true}" = "true" ]; then
 fi
 dnf5 -y clean all
 
-./ctx/download-github.sh https://github.com/itotm/eleven-twilight/releases/download/1.0/ElevenTwilight.tar.gz /usr/share/icons
+./ctx/download-github.sh https://github.com/itotm/eleven-twilight/releases/download/1.0/ElevenTwilight.tar.gz /usr/share/icons > /dev/null
 sleep 1
-./ctx/download-github.sh https://github.com/itotm/plasma-colors/releases/download/v1.0/ClearSimple.colors.tar.gz /usr/share/color-schemes
+./ctx/download-github.sh https://github.com/itotm/plasma-colors/releases/download/v1.0/ClearSimple.colors.tar.gz /usr/share/color-schemes > /dev/null
 sleep 1
-./ctx/download-github.sh https://github.com/itotm/plasma-colors/releases/download/v1.0/ClearSimple.tar.gz /usr/share/plasma/desktoptheme/ClearSimple
+./ctx/download-github.sh https://github.com/itotm/plasma-colors/releases/download/v1.0/ClearSimple.tar.gz /usr/share/plasma/desktoptheme/ClearSimple > /dev/null
 sleep 1
-./ctx/download-github.sh https://github.com/itotm/plasma-colors/releases/download/v1.0/colored-plasma-logo.tar.gz /usr/share/plasma/look-and-feel/colored-plasma-logo
+./ctx/download-github.sh https://github.com/itotm/plasma-colors/releases/download/v1.0/colored-plasma-logo.tar.gz /usr/share/plasma/look-and-feel/colored-plasma-logo > /dev/null
 sleep 1
-./ctx/download-github.sh https://github.com/itotm/plasma-colors/tree/main/Konsole /usr/share/konsole
+./ctx/download-github.sh https://github.com/itotm/plasma-colors/tree/main/Konsole /usr/share/konsole > /dev/null
 
 systemctl enable sshd
 systemctl enable podman.socket
@@ -23,15 +23,11 @@ cp -r /ctx/sys_files/* /
 systemctl enable horizon-setup-system.service
 systemctl enable horizon-update.timer
 
-BUILD_DATE=$(date +'%Y%m%d')
-BUILD_NUMBER=${BUILD_NUMBER:-1}
-VERSION="v${BUILD_DATE}.${BUILD_NUMBER}"
-
 cat > /etc/xdg/kcm-about-distrorc <<EOF
 [General]
-Variant=HorizonOS ${VERSION}
+Variant=HorizonOS ${IMAGE_VERSION}
 Website=https://github.com/itotm/horizon-os
 EOF
 
-echo "${VERSION}" > /etc/horizon-version
+echo "${IMAGE_VERSION}" > /etc/horizon-version
 
