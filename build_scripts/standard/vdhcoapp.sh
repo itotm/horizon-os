4f99c5a314c33ca7d698e5b46de73ff84df5325e
itotm <1804363+itotm@users.noreply.github.com>
2025-12-11 11:43:10 +0100

    Update vdhcoapp.sh to exclude weak dependencies

--- build_scripts/standard/vdhcoapp.sh
+++ build_scripts/standard/vdhcoapp.sh
@@ -2,7 +2,7 @@
 set -oue pipefail
 
 dnf5 -y copr enable rok/vdhcoapp
-dnf5 -y install vdhcoapp
+dnf5 -y install --setopt=install_weak_deps=0 vdhcoapp
 dnf5 -y copr disable rok/vdhcoapp
 
 vdhcoapp install
