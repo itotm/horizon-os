95407cebcf34f7c4d6795eefe37f07376dd33343
itotm <1804363+itotm@users.noreply.github.com>
2025-12-12 10:45:42 +0100

    Simplify epson-v200 installation script by directly installing RPMs without temporary file

--- build_scripts/standard/epson-v200.sh
+++ build_scripts/standard/epson-v200.sh
@@ -8,9 +8,6 @@ DIR="iscan-gt-f670-bundle-2.30.4.x64.rpm"
 curl -LO "$URL"
 tar xzf "$ARCHIVE"
 
-find "$DIR" -type f -name '*.rpm' > /tmp/epson-v200-rpms.txt
+rpm --nosignature --nodigest -i $(find "$DIR" -type f -name '*.rpm')
 
-dnf5 -y install --nogpgcheck $(cat /tmp/epson-v200-rpms.txt)
-
-rm -f /tmp/epson-v200-rpms.txt
 rm -rf "$ARCHIVE" "$DIR"
