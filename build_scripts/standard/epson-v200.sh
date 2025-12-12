1870af5d1dc324ff953c470305eb94d93d14e092
itotm <1804363+itotm@users.noreply.github.com>
2025-12-12 11:59:05 +0100

    Refactor epson-v200 installation script to specify RPM package names and order for installation

--- build_scripts/standard/epson-v200.sh
+++ build_scripts/standard/epson-v200.sh
@@ -8,6 +8,8 @@ DIR="iscan-gt-f670-bundle-2.30.4.x64.rpm"
 curl -LO "$URL"
 tar xzf "$ARCHIVE"
 
-rpm --nosignature --nodigest -i $(find "$DIR" -type f -name '*.rpm')
+rpm --nosignature --nodigest -i $(find "$DIR" -type f -name 'iscan-data-*.noarch.rpm' | sort)
+rpm --nosignature --nodigest -i $(find "$DIR" -type f -name 'iscan-*.x86_64.rpm' ! -name 'iscan-plugin-gt-f670-*.x86_64.rpm' | sort)
+rpm --nosignature --nodigest -i $(find "$DIR" -type f -name 'iscan-plugin-gt-f670-*.x86_64.rpm' | sort)
 
 rm -rf "$ARCHIVE" "$DIR"
