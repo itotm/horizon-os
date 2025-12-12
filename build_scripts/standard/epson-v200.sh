0c02cb1f6670a68274ffba32b847c4e67ba7cd4f
itotm <1804363+itotm@users.noreply.github.com>
2025-12-12 10:19:26 +0100

    Refactor epson-v200 installation script to use dnf for RPM installation

--- build_scripts/standard/epson-v200.sh
+++ build_scripts/standard/epson-v200.sh
@@ -7,9 +7,11 @@ DIR="iscan-gt-f670-bundle-2.30.4.x64.rpm"
 
 curl -LO "$URL"
 tar xzf "$ARCHIVE"
-cd "$DIR"
 
-sudo rpm --nosignature --nodigest -i ./*.rpm
-cd ..
+find "$DIR" -type f -name '*.rpm' > /tmp/epson-v200-rpms.txt
+
+sudo dnf install -y --nogpgcheck --nosignature --nodigest $(cat /tmp/epson-v200-rpms.txt)
+
+rm -f /tmp/epson-v200-rpms.txt
 
 rm -rf "$ARCHIVE" "$DIR"
