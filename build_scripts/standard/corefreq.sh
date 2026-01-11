ee05101190e071fe90df9962e771560e14ac40ae
itotm <1804363+itotm@users.noreply.github.com>
2026-01-11 11:54:52 +0100

    Update corefreq installation and add kernel module handling
    Install kernel-devel alongside corefreq and handle kernel modules.

--- build_scripts/standard/corefreq.sh
+++ build_scripts/standard/corefreq.sh
@@ -2,5 +2,14 @@
 set -oue pipefail
 
 dnf5 -y copr enable sunnyyang/corefreq
-dnf5 -y install corefreq
+dnf5 -y install corefreq kernel-devel
 dnf5 -y copr disable sunnyyang/corefreq
+
+for KVER in /usr/lib/modules/*; do
+    if [ -d "$KVER" ]; then
+        KERNEL_VERSION=$(basename "$KVER")
+        akmods --force --kernels "${KERNEL_VERSION}"
+    fi
+done
+
+depmod -a
