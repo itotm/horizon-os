83a9cefb44a24c1961ede4ee53d51df21abb9940
itotm <1804363+itotm@users.noreply.github.com>
2026-01-13 05:13:24 +0100

    Update corefreq.sh

--- build_scripts/standard/corefreq.sh
+++ build_scripts/standard/corefreq.sh
@@ -2,7 +2,7 @@
 set -oue pipefail
 
 dnf5 -y copr enable sunnyyang/corefreq
-dnf5 -y install corefreq kernel-devel
+dnf5 -y install corefreq
 dnf5 -y copr disable sunnyyang/corefreq
 
 KERNEL_VERSION=$(ls -1 /usr/lib/modules/ | head -n1)
@@ -11,4 +11,4 @@ akmods --force --kernels "${KERNEL_VERSION}" 2>&1 | grep -v "could not open dire
 
 depmod -a "${KERNEL_VERSION}" 2>&1 | grep -v "azure" || true
 
-dnf5 -y remove kernel-devel
+dnf5 -y remove corefreq
