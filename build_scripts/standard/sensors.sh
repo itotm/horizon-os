640390f2b5e5961b6495f2f8c56e3ba985f827db
itotm <1804363+itotm@users.noreply.github.com>
2026-01-17 15:54:10 +0100

    Refactor sensors.sh to include additional development tools and streamline RPM build directory setup

--- build_scripts/standard/sensors.sh
+++ build_scripts/standard/sensors.sh
@@ -2,31 +2,23 @@
 set -oue pipefail
 
 # nct6687d
-dnf5 -y install kernel-devel git gcc make diffstat doxygen gettext patch patchutils subversion systemtap rpmdevtools kmodtool
+dnf5 -y install kernel-devel kernel-headers dkms git gcc gcc-c++ make automake diffstat doxygen gettext patch patchutils subversion systemtap rpmdevtools kmodtool
 
 BUILD_DIR="/tmp/nct6687d-build"
-RPMBUILD_DIR="/tmp/rpmbuild"
 mkdir -p "${BUILD_DIR}"
-mkdir -p "${RPMBUILD_DIR}"/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-cd "${BUILD_DIR}"
 
-cat > ~/.rpmmacros << EOF
-%_topdir ${RPMBUILD_DIR}
-EOF
+cd "${BUILD_DIR}"
 
 git clone https://github.com/Fred78290/nct6687d.git
 cd nct6687d
 
-mkdir -p ~/rpmbuild/{build,rpms,sources,specs,srpms}
-make akmod
+make install
 
 mkdir -p /etc/modules-load.d
 echo "nct6687" >> /etc/modules-load.d/nct6687.conf
 
 cd /
 rm -rf "${BUILD_DIR}"
-rm -rf "${RPMBUILD_DIR}"
-rm -f ~/.rpmmacros
 
 # corefreq
 dnf5 -y copr enable sunnyyang/corefreq
@@ -39,4 +31,4 @@ akmods --force --kernels "${KERNEL_VERSION}" 2>&1 || true
 
 depmod -a "${KERNEL_VERSION}" 2>&1 || true
 
-dnf5 -y remove kernel-devel git gcc make diffstat doxygen gettext patch patchutils subversion systemtap rpmdevtools kmodtool
+dnf5 -y remove kernel-devel kernel-headers dkms git gcc gcc-c++ make automake diffstat doxygen gettext patch patchutils subversion systemtap rpmdevtools kmodtool
