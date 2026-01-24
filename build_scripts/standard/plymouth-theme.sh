622ff26b7dedb8a71f25bfedcb38563eeb397ee6
itotm <1804363+itotm@users.noreply.github.com>
2026-01-24 10:03:29 +0100

    Comment out plymouth-set-default-theme command in plymouth-theme script

--- build_scripts/standard/plymouth-theme.sh
+++ build_scripts/standard/plymouth-theme.sh
@@ -3,4 +3,4 @@ set -oue pipefail
 
 ./ctx/download-github.sh https://github.com/itotm/plymouth-themes/releases/download/v1.0/fedora-logo.tar.gz /usr/share/plymouth/themes
 
-plymouth-set-default-theme fedora-logo
+#plymouth-set-default-theme fedora-logo
