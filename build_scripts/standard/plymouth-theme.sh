cb8932fe6ce4ce93e108021f47ec557fa67a2c1e
itotm <1804363+itotm@users.noreply.github.com>
2026-01-24 08:37:14 +0100

    Remove unnecessary rpm-ostree command from plymouth-theme script

--- build_scripts/standard/plymouth-theme.sh
+++ build_scripts/standard/plymouth-theme.sh
@@ -4,5 +4,3 @@ set -oue pipefail
 ./ctx/download-github.sh https://github.com/itotm/plymouth-themes/releases/download/v1.0/fedora-logo.tar.gz /usr/share/plymouth/themes
 
 plymouth-set-default-theme fedora-logo
-
-rpm-ostree initramfs --enable
