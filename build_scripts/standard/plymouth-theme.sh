033230c157b8919f0676bee3d632ff19fb645392
itotm <1804363+itotm@users.noreply.github.com>
2026-01-25 10:18:40 +0100

    Uncomment plymouth-set-default-theme command and add initramfs regeneration logic

--- build_scripts/standard/plymouth-theme.sh
+++ build_scripts/standard/plymouth-theme.sh
@@ -3,4 +3,17 @@ set -oue pipefail
 
 ./ctx/download-github.sh https://github.com/itotm/plymouth-themes/releases/download/v1.0/fedora-logo.tar.gz /usr/share/plymouth/themes
 
-#plymouth-set-default-theme fedora-logo
+plymouth-set-default-theme fedora-logo
+
+KERNEL_VERSION=$(ls -1 /usr/lib/modules/ | head -n1)
+INITRAMFS_IMAGE="/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
+echo "Starting initramfs regeneration for kernel version: ${KERNEL_VERSION}"
+    "${DRACUT}" \
+    --kver "${KERNEL_VERSION}" \
+    --force \
+    --add 'ostree' \
+    --no-hostonly \
+    --reproducible \
+    "${INITRAMFS_IMAGE}"
+
+chmod 0600 "${INITRAMFS_IMAGE}"
