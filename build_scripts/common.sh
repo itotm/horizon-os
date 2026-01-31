750aab824da8efbd670cd01aa3a08f2f54ad5cba
itotm <1804363+itotm@users.noreply.github.com>
2026-01-31 19:20:20 +0100

    Reorganize installation scripts

--- build_scripts/common.sh
+++ build_scripts/common.sh
@@ -33,6 +33,6 @@ Variant=HorizonOS ${IMAGE_VERSION}
 Website=https://github.com/itotm/horizon-os
 EOF
 
-sed -i "s/^PRETTY_NAME=\"/PRETTY_NAME=\"HorizonOS ${IMAGE_VERSION} - /" /etc/os-release
+sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME=\"HorizonOS ${IMAGE_VERSION}\"/" /etc/os-release
 
 echo "${IMAGE_VERSION}" > /etc/horizon-version
