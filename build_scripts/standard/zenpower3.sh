ed176701ace190f76dbff2e73ee6734d462edf7d
itotm <1804363+itotm@users.noreply.github.com>
2026-02-01 16:49:41 +0100

    Update zenpower3 installation script to include kernel-devel and add kernel module handling

--- build_scripts/standard/zenpower3.sh
+++ build_scripts/standard/zenpower3.sh
@@ -2,5 +2,13 @@
 set -oue pipefail
 
 dnf5 -y copr enable shdwchn10/zenpower3
-dnf5 -y install zenpower3 zenmonitor3
+dnf5 -y install kernel-devel zenpower3 zenmonitor3
 dnf5 -y copr disable shdwchn10/zenpower3
+
+KERNEL_VERSION=$(ls -1 /usr/lib/modules/ | head -n1)
+
+akmods --force --kernels "${KERNEL_VERSION}" 2>&1 || true
+
+depmod -a "${KERNEL_VERSION}" 2>&1 || true
+
+dnf5 -y remove kernel-devel
