08d63796259c9d8e970871d000a2528f2115127c
itotm <1804363+itotm@users.noreply.github.com>
2026-02-01 17:49:44 +0100

    Add ublue installation script to standard directory

--- build_scripts/standard/ublue.sh
+++ build_scripts/standard/ublue.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -oue pipefail
 
+dnf5 -y install kernel-devel
+
 dnf5 -y copr enable ublue-os/akmods
-dnf5 -y install akmod-zenpower3 akmod-nct6687d kernel-devel
+dnf5 -y install --setopt=tsflags=noscripts akmod-zenpower3 akmod-nct6687d
 dnf5 -y copr disable ublue-os/akmods
 
 KERNEL_VERSION=$(ls -1 /usr/lib/modules/ | head -n1)
