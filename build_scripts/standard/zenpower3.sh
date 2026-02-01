01550620474b42bf9d070e095964c0d65f207ec8
itotm <1804363+itotm@users.noreply.github.com>
2026-02-01 17:19:52 +0100

    Refactor zenpower3 installation script

--- build_scripts/standard/zenpower3.sh
+++ build_scripts/standard/zenpower3.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -oue pipefail
 
+dnf5 -y install kernel-devel
+
 dnf5 -y copr enable shdwchn10/zenpower3
-dnf5 -y install kernel-devel zenpower3 zenmonitor3
+dnf5 -y install --setopt=tsflags=noscripts akmod-zenpower3
+dnf5 -y install zenmonitor3
 dnf5 -y copr disable shdwchn10/zenpower3
 
 KERNEL_VERSION=$(ls -1 /usr/lib/modules/ | head -n1)
