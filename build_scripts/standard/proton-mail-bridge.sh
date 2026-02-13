a0b5a2c615e3e4d2cebfcf36517ba20c00c98629
itotm <1804363+itotm@users.noreply.github.com>
2026-02-13 11:06:03 +0100

    Add script to install ProtonMail Bridge

---
+++ build_scripts/standard/proton-mail-bridge.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -oue pipefail
+
+BRIDGE_VERSION="3.13.0"
+BRIDGE_RPM="protonmail-bridge-${BRIDGE_VERSION}-1.x86_64.rpm"
+BRIDGE_URL="https://proton.me/download/bridge/${BRIDGE_RPM}"
+
+curl -fsSL -o "/tmp/${BRIDGE_RPM}" "${BRIDGE_URL}"
+dnf5 -y install "/tmp/${BRIDGE_RPM}"
+rm -f "/tmp/${BRIDGE_RPM}"
