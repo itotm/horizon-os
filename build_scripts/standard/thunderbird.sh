0dccd5a303ba3ff3de574c6bed4493f01fb17bd4
itotm <1804363+itotm@users.noreply.github.com>
2026-02-14 14:00:55 +0100

    Add workaround to force X11 backend for Thunderbird

--- build_scripts/standard/thunderbird.sh
+++ build_scripts/standard/thunderbird.sh
@@ -4,6 +4,12 @@ set -oue pipefail
 dnf5 config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:/Ximi1970:/Mozilla:/Add-ons/Fedora_43/home:Ximi1970:Mozilla:Add-ons.repo
 dnf5 -y install thunderbird systray-x
 
+# Workaround: force X11 backend for Thunderbird
+DESKTOP_FILE="/usr/share/applications/net.thunderbird.Thunderbird.desktop"
+if grep -q '^Exec=' "${DESKTOP_FILE}"; then
+    sed -i 's|^Exec=|Exec=env GDK_BACKEND=x11 |' "${DESKTOP_FILE}"
+fi
+
 BRIDGE_VERSION="3.21.2"
 BRIDGE_RPM="protonmail-bridge-${BRIDGE_VERSION}-1.x86_64.rpm"
 BRIDGE_URL="https://proton.me/download/bridge/${BRIDGE_RPM}"
