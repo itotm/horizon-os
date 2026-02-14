cf50cac42a277bb2926be85c8c30aebf640cda01
itotm <1804363+itotm@users.noreply.github.com>
2026-02-14 12:18:10 +0100

    Move ProtonMail Bridge installation script to disabled directory and integrate installation into Thunderbird script

--- build_scripts/standard/thunderbird.sh
+++ build_scripts/standard/thunderbird.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -oue pipefail
 
-dnf5 -y install thunderbird
-
 dnf5 config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:/Ximi1970:/Mozilla:/Add-ons/Fedora_43/home:Ximi1970:Mozilla:Add-ons.repo
-dnf5 -y install systray-x
+dnf5 -y install thunderbird systray-x
+
+BRIDGE_VERSION="3.21.2"
+BRIDGE_RPM="protonmail-bridge-${BRIDGE_VERSION}-1.x86_64.rpm"
+BRIDGE_URL="https://proton.me/download/bridge/${BRIDGE_RPM}"
+
+curl -fsSL -o "/tmp/${BRIDGE_RPM}" "${BRIDGE_URL}"
+dnf5 -y install "/tmp/${BRIDGE_RPM}"
+rm -f "/tmp/${BRIDGE_RPM}"
