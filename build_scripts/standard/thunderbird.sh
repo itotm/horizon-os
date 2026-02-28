46100fee5aeef0b00839a56960f49a7574dcc984
itotm <1804363+itotm@users.noreply.github.com>
2026-02-28 09:57:24 +0100

    Update ProtonMail Bridge to version 3.22.0

--- build_scripts/standard/thunderbird.sh
+++ build_scripts/standard/thunderbird.sh
@@ -4,13 +4,13 @@ set -oue pipefail
 dnf5 config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:/Ximi1970:/Mozilla:/Add-ons/Fedora_43/home:Ximi1970:Mozilla:Add-ons.repo
 dnf5 -y install thunderbird systray-x
 
-# Workaround: force X11 backend for Thunderbird
+# force X11 backend for Thunderbird to make systray-x work
 DESKTOP_FILE="/usr/share/applications/net.thunderbird.Thunderbird.desktop"
 if grep -q '^Exec=' "${DESKTOP_FILE}"; then
     sed -i 's|^Exec=|Exec=env GDK_BACKEND=x11 |' "${DESKTOP_FILE}"
 fi
 
-BRIDGE_VERSION="3.21.2"
+BRIDGE_VERSION="3.22.0"
 BRIDGE_RPM="protonmail-bridge-${BRIDGE_VERSION}-1.x86_64.rpm"
 BRIDGE_URL="https://proton.me/download/bridge/${BRIDGE_RPM}"
 
