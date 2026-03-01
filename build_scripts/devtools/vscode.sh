fc48ca8d2886071d32589c57ac675645c944d3af
itotm <1804363+itotm@users.noreply.github.com>
2026-03-01 08:10:52 +0100

    Update vscode.sh

--- build_scripts/devtools/vscode.sh
+++ build_scripts/devtools/vscode.sh
@@ -4,5 +4,4 @@ set -oue pipefail
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 
-dnf5 check-update
 dnf5 -y install code
