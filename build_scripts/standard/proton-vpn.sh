c2f5b5c988e139ab7ada810a8902ae7f19573d44
itotm <1804363+itotm@users.noreply.github.com>
2026-04-12 13:55:06 +0200

    Simplify Proton VPN installation script by removing unnecessary package array

--- build_scripts/standard/proton-vpn.sh
+++ build_scripts/standard/proton-vpn.sh
@@ -2,12 +2,4 @@
 set -oue pipefail
 
 dnf5 -y install https://repo.protonvpn.com/fedora-$(rpm -E %fedora)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm
-
-PROTON_PACKAGES=(
-    proton-vpn-cli
-    proton-vpn-gnome-desktop
-    libappindicator-gtk3
-    gnome-shell-extension-appindicator
-    gnome-extensions-app
-)
-dnf5 -y install "${PROTON_PACKAGES[@]}"
+dnf5 -y install proton-vpn-cli
