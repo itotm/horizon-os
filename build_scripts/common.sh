434ae1109cc5bdfae26ddac2589cb22c716e1744
itotm <1804363+itotm@users.noreply.github.com>
2026-05-05 18:38:29 +0200

    Remove plpugin download in common.sh

--- build_scripts/common.sh
+++ build_scripts/common.sh
@@ -18,11 +18,7 @@ sleep 1
 sleep 1
 ./ctx/download-github.sh https://github.com/itotm/plasma-colors/releases/download/v1.0/breeze-cursors-light-plasma5.tar.gz /usr/share/icons
 sleep 1
-./ctx/download-github.sh https://github.com/itotm/plasma-colors/tree/main/Konsole /usr/share/konsole > /dev/null
-sleep 1
-./ctx/download-github.sh https://github.com/itotm/plasma-wallpaper-potd-enhanced/releases/download/v1.3.2/plasma-wallpaper-potd-enhanced-1.3.2.tar.gz /usr/share/plasma/wallpapers > /dev/null
-sleep 1
-./ctx/download-github.sh https://github.com/itotm/kickoff-simplified/releases/download/v1.2/kickoff-simplified-1.2.tar.gz /usr/share/plasma/plasmoids > /dev/null
+./ctx/download-github.sh https://github.com/itotm/plasma-colors/tree/main/Konsole /usr/share/konsole
 
 cp -r /ctx/sys_files/* /
 systemctl enable horizon-setup-system.service
