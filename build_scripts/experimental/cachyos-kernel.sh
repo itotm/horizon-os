e1494a2d7d5f4dcf286a8458928053b985920817
itotm <1804363+itotm@users.noreply.github.com>
2026-05-21 19:42:25 +0200

    Refactor cachyos-kernel.sh

--- build_scripts/experimental/cachyos-kernel.sh
+++ build_scripts/experimental/cachyos-kernel.sh
@@ -3,9 +3,21 @@ set -oue pipefail
 
 dnf5 -y copr enable bieszczaders/kernel-cachyos-lto
 
-rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra \
-    --install kernel-cachyos-lto
+mv /usr/lib/kernel/install.d/05-rpmostree.install \
+   /usr/lib/kernel/install.d/05-rpmostree.install.disabled
 
-ostree container commit
+dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+dnf5 -y install kernel-cachyos-lto
+
+mv /usr/lib/kernel/install.d/05-rpmostree.install.disabled \
+   /usr/lib/kernel/install.d/05-rpmostree.install
+
+KVER=$(ls /usr/lib/modules/ | grep cachyos | head -1)
+echo "Kernel: ${KVER}"
+
+depmod -a "${KVER}"
+kernel-install add "${KVER}" /usr/lib/modules/${KVER}/vmlinuz
+
+setsebool -P domain_kernel_load_modules on
 
 dnf5 -y copr disable bieszczaders/kernel-cachyos-lto
