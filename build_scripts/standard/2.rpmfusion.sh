612378a8dd8db0953097658ea42a1647c66e5664
itotm <1804363+itotm@users.noreply.github.com>
2026-05-29 11:35:29 +0200

    Update RPM Fusion installation script and change kernel package to use LTO version

--- build_scripts/standard/2.rpmfusion.sh
+++ build_scripts/standard/2.rpmfusion.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -oue pipefail
+
 dnf5 -y install \
     https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
