ec19daf126f6cc2f91627b569443a2538dba7099
itotm <1804363+itotm@users.noreply.github.com>
2026-07-26 09:17:26 +0200

    Refactor terra.sh: remove unused font packages from TERRA_PACKAGES array

--- build_scripts/standard/terra.sh
+++ build_scripts/standard/terra.sh
@@ -4,9 +4,6 @@ set -oue pipefail
 dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
 TERRA_PACKAGES=(
-    cleartype-fonts
-    ms-core-fonts
-    sticky
     tail-tray
 )
 dnf5 -y install "${TERRA_PACKAGES[@]}"
