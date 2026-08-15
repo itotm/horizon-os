45185207747eab0959dc83757ae0eb32de91d27f
itotm <1804363+itotm@users.noreply.github.com>
2026-08-15 13:46:07 +0200

    Correction

--- build_scripts/standard/1.packages.sh
+++ build_scripts/standard/1.packages.sh
@@ -9,7 +9,6 @@ REMOVE_PACKAGES=(
     virtualbox-guest-additions
 )
 dnf5 -y remove "${REMOVE_PACKAGES[@]}"
-export TMPDIR=/var/tmp
 
 dnf5 -y config-manager setopt fedora-cisco-openh264.enabled=1
 
