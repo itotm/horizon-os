d52138facaefbeaa50bae1c2c0ea86068e6396e9
itotm <1804363+itotm@users.noreply.github.com>
2026-08-15 08:41:27 +0200

    Update terra.sh to use main repo only

--- build_scripts/standard/terra.sh
+++ build_scripts/standard/terra.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -oue pipefail
 
-dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-
 TERRA_PACKAGES=(
     cleartype-fonts
     ms-core-fonts
     tail-tray
 )
-dnf5 -y install "${TERRA_PACKAGES[@]}"
+
+dnf5 -y install \
+    --nogpgcheck \
+    --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' \
+    "${TERRA_PACKAGES[@]}"
