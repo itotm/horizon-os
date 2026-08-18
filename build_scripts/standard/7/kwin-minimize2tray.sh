0eab6c47970e97d074808f1909c89df8dd7903c6
itotm <1804363+itotm@users.noreply.github.com>
2026-08-18 17:49:13 +0200

    Fix package manager command in kwin-minimize2tray installation script

--- build_scripts/standard/7/kwin-minimize2tray.sh
+++ build_scripts/standard/7/kwin-minimize2tray.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-dnf -y copr enable itotm/kwin-minimize2tray
+dnf5 -y copr enable itotm/kwin-minimize2tray
 dnf5 -y install kwin-minimize2tray
+dnf5 -y copr disable itotm/kwin-minimize2tray
