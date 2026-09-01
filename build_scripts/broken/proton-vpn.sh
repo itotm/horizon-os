9dc0bfa597e21a3a71a72f3e1b3e3c4cab753677
itotm <1804363+itotm@users.noreply.github.com>
2026-09-01 08:10:46 +0200

    Add Proton VPN installation script

--- build_scripts/broken/proton-vpn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -oue pipefail
-
-dnf5 -y install https://repo.protonvpn.com/fedora-$(rpm -E %fedora)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm
-dnf5 -y install proton-vpn-cli
