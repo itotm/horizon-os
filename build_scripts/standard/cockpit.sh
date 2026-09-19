1fd35f6361f07e7ea1cc8c3067e6708e8bc63982
itotm <1804363+itotm@users.noreply.github.com>
2026-09-19 13:31:19 +0200

    Add cockpit-sensors installation to cockpit.sh script

--- build_scripts/standard/cockpit.sh
+++ build_scripts/standard/cockpit.sh
@@ -3,7 +3,14 @@ set -oue pipefail
 
 COCKPIT_PACKAGES=(
     cockpit
+    cockpit-files
     cockpit-podman
     cockpit-selinux
 )
 dnf5 -y install "${COCKPIT_PACKAGES[@]}"
+
+curl --http1.1 --retry 3 --retry-delay 10 -fsSL -o /tmp/cockpit-sensors.tar.xz \
+  "https://github.com/ocristopfer/cockpit-sensors/releases/latest/download/cockpit-sensors.tar.xz"
+mkdir -p /usr/share/cockpit/sensors
+tar -xJf /tmp/cockpit-sensors.tar.xz -C /usr/share/cockpit/sensors --strip-components=2 cockpit-sensors/dist
+rm -f /tmp/cockpit-sensors.tar.xz
